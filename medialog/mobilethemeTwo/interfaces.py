0383b029deac5e0957a4c0e09c1269909cfacd6f
Espen Moe-Nilssen <espen@medialog.no>
2014-06-20 16:37:01 +0200

    another try

--- medialog/mobilethemeTwo/interfaces.py
+++ medialog/mobilethemeTwo/interfaces.py
@@ -1,6 +1,5 @@
-from zope.interface import Interface
+from plone.theme.interfaces import IDefaultPloneLayer
 
-
-class ICustomTheme(Interface):
-    """Marker interface that defines a Zope 3 browser layer.
+class IThemeSpecific(IDefaultPloneLayer):
+    """Marker interface that defines a Zope 3 browser layer and a plone skin marker.
         """
